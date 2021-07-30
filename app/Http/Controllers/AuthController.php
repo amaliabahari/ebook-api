755698d6c74a826b382185173c2200f8228433a9
amaliabahari <amaliabahari9@gmail.com>
2021-07-30 11:02:32 +0700

    first commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function me(){
+        return"Biodata : 3103119016, Amalia Bahari, Perempuan, 085741156753, XII RPL 1";
+    }
+}
